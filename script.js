b972a36eef1cfcddd812f01cc479f387f810b60e
Rasul1L <baidaev.rasul00@gmail.com>
2026-06-09 20:33:05 -0700

    Change7

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+window.addEventListener("load", () => {
+  const intro = document.querySelector(".cinematic-intro");
+
+  if (intro) {
+    window.setTimeout(() => {
+      intro.remove();
+    }, 6400);
+  }
+});
+
 function sayHello() {
   alert("Hello! Your website is working.");
 }
